6f2f31e6c39a9d5ef2df94b71a6b897c79b0c686
Pascal Uchubilo <pascalskillz@gmail.com>
2019-10-17 01:06:47 -0400

    update menuitem fields

--- src/main/java/com/monmouthvalley/tandoor/entity/MenuItem.java
+++ src/main/java/com/monmouthvalley/tandoor/entity/MenuItem.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 @Table(name = "menuItem")
 public class MenuItem {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -22,12 +21,37 @@ public class MenuItem {
     @Column(name = "cook_time")
     private int cookTime;
 
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "special")
+    private boolean isSpecial;
+
+    @Column(name = "vegan")
+    private boolean isVagen;
+
+    @Column(name = "description")
+    private String description;
+
     @Column(name = "image_url")
     private String imageUrl;
 
 
     public  MenuItem(){
 
+    }
+
+    public MenuItem(String itemName, double itemPrice, int cookTime, String category,
+                    boolean isSpecial, boolean isVagen, String description, String imageUrl) {
+
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.cookTime = cookTime;
+        this.category = category;
+        this.isSpecial = isSpecial;
+        this.isVagen = isVagen;
+        this.description = description;
+        this.imageUrl = imageUrl;
     }
 
     public MenuItem(String itemName, double itemPrice, int cookTime, String imageUrl) {
@@ -69,6 +93,38 @@ public class MenuItem {
         this.cookTime = cookTime;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
+
+    public void setSpecial(boolean special) {
+        isSpecial = special;
+    }
+
+    public boolean isVagen() {
+        return isVagen;
+    }
+
+    public void setVagen(boolean vagen) {
+        isVagen = vagen;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getImageUrl() {
         return imageUrl;
     }
@@ -76,6 +132,8 @@ public class MenuItem {
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
+
 
     @Override
     public String toString() {
